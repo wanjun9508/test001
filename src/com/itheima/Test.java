1ceeb270b51966155d51d4ae4adab6cfa32963d3
Wanjun <Wanjun@qq.com>
2018-08-11 20:57:27 +0800

    第一次提交

---
+++ src/com/itheima/Test.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+/**
+ * 描述:
+ *
+ * @author admin
+ * @version 1.0
+ * @classname Test
+ * @create 2018-08-11 20:51
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好,世界");
+    }
+}
